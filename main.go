eee45f74ccb3c6f42e4582c18d08179c58ef2f5b
Jett Krassner <jett@Jetts-MacBook-Air.local>
2019-03-07 15:02:29 -0800

    first commit: added readme, gitignore, main

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+// import pkgs
+import "fmt"
+
+func main() {
+	fmt.Println("Uh Suhhh!")
+	lit()
+
+	fmt.Println("Bouta print \"lit\" during \"even\" and running 77 times!")
+
+	// notice the parenthesis not used, no need
+	// stop using equal signs!!! ue := for var assignments
+	for i := 0; i < 77; i++ {
+		fmt.Print(i + 1)
+		fmt.Print(": ")
+
+		if i%2 == 0 {
+			lit()
+		} else {
+			tight()
+		}
+
+	}
+
+	// exit
+	tight()
+}
+
+func lit() {
+	fmt.Println("This is lit!")
+}
+
+func tight() {
+	fmt.Println("This is tight!")
+}
+
+func baii() {
+	fmt.Println("Exiting")
+}
