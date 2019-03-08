4ecb2fe509e05e36525fad011c6ccd8f8af1ba0e
Jett Krassner <jett@Jetts-MacBook-Air.local>
2019-03-07 16:51:34 -0800

    exercise folder added, jedi-level1-exercise.go file added for hands on exercises

---
+++ 008-01-exercises/jedi-level1-exercise.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// ****************** EXERCISE 1 ******************
+	// Declare and assign values
+	x := 42
+	y := "James Bond"
+	z := true
+
+	// single print
+	fmt.Printf("x = %v\ny = %v\nz = %v\n", x, y, z)
+
+	// multiple print
+	fmt.Println("x =", x)
+	fmt.Println("y =", y)
+	fmt.Println("z =", z)
+
+	// ****************** EXERCISE 2 ******************
+
+	// ****************** EXERCISE 3 ******************
+
+	// ****************** EXERCISE 4 ******************
+}
