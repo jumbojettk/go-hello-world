fcc6581ddde9c2a5bf3ebbbfb2245d61b46f8ba7
Jett Krassner <jett@Jetts-MacBook-Air.local>
2019-04-04 10:09:02 -0700

    playground 4: working with maps

--- playground-4/main.go
+++ playground-4/main.go
@@ -51,4 +51,76 @@ func main() {
 	x = append(x[:4], x[5:11]...)
 	fmt.Println(x)
 
+	// use make to declare array if size is known
+	// make array of int with size 10 but cap at 15
+	j := make([]int, 10, 15)
+
+	// load numbers from 1 to 10 and print
+	for i := 0; i < len(j); i++ {
+		j[i] = i + 1
+	}
+	fmt.Println("array:", j, "size:", len(j), "cap:", cap(j))
+
+	// append until size 16 and print
+	j = append(j, 11, 12, 13, 14, 15, 16)
+	fmt.Println("array:", j, "size:", len(j), "cap:", cap(j))
+
+	// multi-dimensional slice
+	// declare 2 slices
+	firstSlice := []string{"Naruto", "Luffy", "Natsu"}
+	secondSlice := []string{"Simba", "Tmone", "Pumba"}
+	fmt.Println("firstSlice:", firstSlice, "secondSlice:", secondSlice)
+
+	allSlices := [][]string{firstSlice, secondSlice}
+	fmt.Println("allSlices:", allSlices)
+
+	// working with maps
+	// declare map of string as index and int as value
+	m := map[string]int{
+		"Jett":      25,
+		"Jelly":     22,
+		"Frank":     70,
+		"Piyawadee": 50,
+	}
+	// print the map
+	fmt.Println("map:", m)
+
+	// add a new key:value to the map and print
+	m["Sam"] = 23
+	fmt.Println("map:", m)
+
+	// access map using comma ok idiom
+	if v, ok := m["Jett"]; ok {
+		fmt.Println(v, "is an awesome age! - Jett")
+	}
+
+	// access map using comma ok idiom for something not found
+	if v, ok := m["John"]; ok {
+		fmt.Println(v, "is an awesome age! - John")
+	} else {
+		fmt.Println("Couldn't find John")
+	}
+
+	// add John and print
+	m["John"] = 32
+	if v, ok := m["John"]; ok {
+		fmt.Println(v, "is an awesome age! (after added) - John")
+	}
+
+	// use range to iterate through the whole map
+	for key, value := range m {
+		fmt.Println(key, ":", value)
+	}
+
+	// delete map using comma ok idiom
+	if v, ok := m["John"]; ok {
+		fmt.Println("Bye John whose", v)
+		delete(m, "John")
+	}
+
+	// print map again
+	for key, value := range m {
+		fmt.Println(key, ":", value)
+	}
+
 }
